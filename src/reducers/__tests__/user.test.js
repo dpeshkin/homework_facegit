91ad30a26c51be981b66e49b4856df334d7c7c9e
DmitryPeshkin <Dmitrey04@mail.ru>
2017-12-03 23:10:32 +0500

    added user reducer tests

---
+++ src/reducers/__tests__/user.test.js
@@ -0,0 +1,62 @@
+import users from "../users";
+import {
+  fetchUserRequest,
+  fetchUserSuccess,
+  fetchUserFailure
+} from "../../actions/users";
+
+describe("В редьюсере users", () => {
+  describe("Экшен fetchUserRequest", () => {
+    it("изменяет флаг isFetching на true", () => {
+      const next = users({ isFetching: false }, fetchUserRequest());
+      expect(next.isFetching).toBeTruthy();
+    });
+
+    it("изменяет флаг error на false", () => {
+      const next = users({ error: true }, fetchUserRequest());
+      expect(next.error).toBeFalsy();
+    });
+
+    it("очищает поле data", () => {
+      const next = users({ data: [] }, fetchUserRequest());
+      expect(next.data).toEqual(null);
+    });
+  });
+
+  describe("Экшен fetchUserSuccess", () => {
+    const payload = { data: { login: "ekb196" } };
+    it("изменяет флаг isFetching на false", () => {
+      const next = users({ isFetching: true }, fetchUserSuccess(payload));
+      expect(next.isFetching).toBeFalsy();
+    });
+
+    it("изменяет флаг error на false", () => {
+      const next = users({ error: true }, fetchUserSuccess(payload));
+      expect(next.error).toBeFalsy();
+    });
+
+    it("наполняет данными data", () => {
+      const next = users(null, fetchUserSuccess(payload));
+      expect(next.data).toEqual(payload.data);
+    });
+
+    describe("Экшен fetchUserFailure", () => {
+      const payload = { data: { login: "ekb196" } };
+      it("изменяет флаг isFetching на false", () => {
+        const next = users({ isFetching: true }, fetchUserFailure());
+        expect(next.isFetching).toBeFalsy();
+      });
+
+      it("изменяет флаг error на true", () => {
+        const next = users({ error: false }, fetchUserFailure());
+        console.log(next.error);
+        expect(next.error).toBeTruthy();
+      });
+
+      it("очищает поле data", () => {
+        const next = users({ data: [] }, fetchUserFailure());
+        expect(next.data).toEqual(null);
+      });
+    });
+  });
+});
--- src/reducers/users.js
+++ src/reducers/users.js
@@ -22,7 +22,8 @@ export default handleActions(
     [fetchUserFailure]: state => ({
       ...state,
       isFetching: false,
-      error: true
+      error: true,
+      data: null
     })
   },
   {
