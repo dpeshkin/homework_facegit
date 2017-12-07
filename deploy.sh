1d7577c42565c58c58d41755d81d75287dce1eff
DmitryPeshkin <Dmitrey04@mail.ru>
2017-12-07 18:34:54 +0500

    test deploy

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
+
 set -e
 zip -r build.zip build
 
 curl -H "Content-type: application/zip" \
      -H "Authorization: Bearer $NETLIFY_KEY" \
-      -d "{\"quiet\": true}" \
      --data-binary "@build.zip" \
      https://api.netlify.com/api/v1/sites
